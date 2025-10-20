0b31a013b26c7b97772a2f07420913ef5b2b55e2
= <=>
2025-10-20 11:37:45 +0300

    Initial design copy from dribbble

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,16 +1,40 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-  isDark = false;
+export class AppComponent implements OnInit, AfterViewInit {
+  public isDark = false;
+  public navLinks: { name: string, url: string, icon?: string }[] = [
+    { name: "Feed", url: "/" },
+    { name: "Messages", url: "/messages" },
+    { name: "Communities", url: "/communities" },
+    { name: "Groups", url: "/groups" },
+    { name: "Feed", url: "/" },
+    { name: "Settings", url: "/settings" },
+  ]
 
   constructor() {
+  }
+
+  ngOnInit() {
+    this.navLinks = [
+    { name: "Feed", url: "/" },
+    { name: "Messages", url: "/messages" },
+    { name: "Communities", url: "/communities" },
+    { name: "Groups", url: "/groups" },
+    { name: "Feed", url: "/" },
+    { name: "Settings", url: "/settings" },
+  ]
+    console.log(this.navLinks)
+  }
+
+  ngAfterViewInit() {
     const stored = localStorage.getItem('theme');
     this.isDark = stored === 'dark';
     this.applyTheme();
